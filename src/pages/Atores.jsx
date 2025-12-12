71ac7b07427a40c9075f008e1ba4929a133491ce
Daniel Perini <daniel@periniprojetos.com.br>
2025-12-12 18:04:31 +0000

    File changes

--- src/pages/Atores.jsx
+++ src/pages/Atores.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { base44 } from '@/api/base44Client';
+import { createPageUrl } from '@/utils';
+import { Link } from 'react-router-dom';
 import { 
   Users, 
   Plus, 
-  Search, 
+  Search,
+  Network, 
   Filter,
   MapPin,
   Phone,
