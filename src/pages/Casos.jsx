8c9a1949afbe9d604e8164588cf310fba63cc787
Daniel Perini <daniel@periniprojetos.com.br>
2025-12-12 22:33:53 +0000

    File changes

--- src/pages/Casos.jsx
+++ src/pages/Casos.jsx
@@ -45,6 +45,12 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
 } from "@/components/ui/alert-dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { cn } from "@/lib/utils";
 
 const statusConfig = {
