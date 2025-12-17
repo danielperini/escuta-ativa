60004595ce7d0a24ae513f69fd8c80fe3fdeef5b
Daniel Perini <daniel@periniprojetos.com.br>
2025-12-17 01:35:31 +0000

    File changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -262,7 +262,6 @@ export default function Dashboard() {
                     <BotaoPanicoAvancado />
                 </div>
             </div>
-            </div>
-            </ErrorBoundary>
-            );
-            }
+        </div>
+    );
+}
